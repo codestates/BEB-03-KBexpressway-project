80798106d641761fbad523b10fa91da32cfb8e60
DESKTOP-IV4FSNI\espre <espressom@naver.com>
2022-04-08 18:02:51 +0900

    [1] 컴포넌트, 페이지 분리 및 라우팅

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Main from "./pages/Main.js";
+import Nav from "./components/Nav.js";
+import Explorer from "./pages/Explorer.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Nav />
+            <Switch>
+                <Route exact path="/">
+                    <Main />
+                </Route>
+                <Route path="/explorer">
+                    <Explorer />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
