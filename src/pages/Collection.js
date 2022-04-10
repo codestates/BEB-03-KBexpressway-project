72255aa87e7c4f93fe0e14b83cc8428ab1c42bc5
espressom <espressom@naver.com>
2022-04-10 23:10:38 +0900

    [7] 지갑 연동 완료

--- src/pages/Collection.js
+++ src/pages/Collection.js
@@ -3,7 +3,7 @@ import ItemListContainer from "../components/ItemListContainer";
 function Collection({ match }) {
   const collectionId = match?.params?.collectionId;
 
-  console.log(`[📦️Collection.js] collectionId 📌️${collectionId}`);
+  // console.log(`[📦️Collection.js] collectionId 📌️${collectionId}`);
 
   return (
     <section id="portfolio" className="portfolio">
