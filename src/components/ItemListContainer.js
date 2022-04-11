51bc05165163a6d27eebd66cc9d3ecd7170cb7e0
Jeehee77 <gnarled47@gmail.com>
2022-04-11 15:16:28 +0900

    디자인 수정 및 ItemDetail->Collection Link 연결

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,33 +1,23 @@
 import Item from "./Item.js";
 import nftData from "../data/nftData.json";
+import collectionData from "../data/collectionData.json";
 
 function ItemListContainer({ collectionId }) {
     console.log(`ItemListContainer 📌️${collectionId}`);
     const nftList = nftData.filter((nft) => {
-        // console.log(nft);
-        return nft.data.collectionId === collectionId;
+        return Number(nft.data.collectionId) === Number(collectionId);
     });
-    console.log(nftList);
+    const collection = collectionData.filter((el) => {
+        return Number(el.collectionId) === Number(collectionId);
+    })[0];
 
     return (
         <div className="container">
         &nbsp;
         <header className="section-header">
             <h2>Collection</h2>
-            <p>Check our latest work</p>
+            <p>{collection.name}</p>
         </header>
-        <div className="row">
-            <div className="col-lg-12 d-flex justify-content-center">
-            <ul id="portfolio-flters">
-                <li data-filter="*" className="filter-active">
-                All
-                </li>
-                <li data-filter=".filter-app">App</li>
-                <li data-filter=".filter-card">Card</li>
-                <li data-filter=".filter-web">Web</li>
-            </ul>
-            </div>
-        </div>
         <div className="row gy-4 portfolio-container">
             {nftList.map((nft) => {
                 return <Item nft={nft}/>
