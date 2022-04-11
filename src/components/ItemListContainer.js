281f14777a56985d59eb93f517bc589ae4d4c532
Jeehee77 <gnarled47@gmail.com>
2022-04-11 15:37:56 +0900

    Collection, ItemListComtainer 컴포넌트 디자인 수정

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,23 +1,16 @@
 import Item from "./Item.js";
 import nftData from "../data/nftData.json";
-import collectionData from "../data/collectionData.json";
 
 function ItemListContainer({ collectionId }) {
     console.log(`ItemListContainer 📌️${collectionId}`);
     const nftList = nftData.filter((nft) => {
         return Number(nft.data.collectionId) === Number(collectionId);
     });
-    const collection = collectionData.filter((el) => {
-        return Number(el.collectionId) === Number(collectionId);
-    })[0];
+    
 
     return (
         <div className="container">
-        &nbsp;
-        <header className="section-header">
-            <h2>Collection</h2>
-            <p>{collection.name}</p>
-        </header>
+        
         <div className="row gy-4 portfolio-container">
             {nftList.map((nft) => {
                 return <Item nft={nft}/>
